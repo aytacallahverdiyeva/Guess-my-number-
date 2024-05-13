218e2ed5c97c5bd75d07de85d23c3304c82c4513
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-05-13 21:00:21 +0400

    version 4 adding logic to check btn

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '🔢 No number! '; 
+    }
+})
