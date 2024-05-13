b476b78f8b20c3af5e1c3942a398d72fb3b6aa41
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-05-13 22:52:08 +0400

    version 5 added several logical lines of code for advanced version

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // console.log(document.querySelector('.message').textContent);
 // document.querySelector('.message').textContent = 'Correct number!';
@@ -9,11 +9,36 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess);
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-    if(!guess) {
-        document.querySelector('.message').textContent = '🔢 No number! '; 
+document.querySelector(".number").textContent = number;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "🔢 No number! ";
+  } else if (guess === number) {
+    document.querySelector(".message").textContent = "🥳 Correct Number!";
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high! ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+        document.querySelector('.message').textContent = '🥲 You lost the game...'
+        document.querySelector('.score').textContent = 0;
     }
-})
+  } else if (guess < number) {
+    if (score > 1) {
+        document.querySelector(".message").textContent = "📉 Too low! ";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+          document.querySelector('.message').textContent = '🥲 You lost the game...'
+          document.querySelector('.score').textContent = 0;
+      }
+  }
+});
