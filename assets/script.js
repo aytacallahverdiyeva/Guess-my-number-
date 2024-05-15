74312f4bf5752eb250de817b5f544e3ce9a6f5df
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-05-15 18:38:28 +0400

    again button functionality and color changing function

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector(".number").textContent = number;
+let highscore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -21,8 +20,20 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent = "🔢 No number! ";
   } else if (guess === number) {
+    document.querySelector(".number").textContent = number;
     document.querySelector(".message").textContent = "🥳 Correct Number!";
-  } else if (guess > number) {
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if(score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
+
+  } 
+  
+  else if (guess > number) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high! ";
       score--;
@@ -31,7 +42,9 @@ document.querySelector(".check").addEventListener("click", function () {
         document.querySelector('.message').textContent = '🥲 You lost the game...'
         document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < number) {
+  } 
+  
+  else if (guess < number) {
     if (score > 1) {
         document.querySelector(".message").textContent = "📉 Too low! ";
         score--;
@@ -42,3 +55,16 @@ document.querySelector(".check").addEventListener("click", function () {
       }
   }
 });
+
+// Again button functionality
+document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".message").textContent = "Start guessing... ";  
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.highscore').textContent = highscore;
+})
